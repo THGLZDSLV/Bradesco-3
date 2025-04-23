e7e084cb5705ef3879b4b1a586efcefa16c9b0c9
THGLZDSLV <thiagoluiz15silva@gmail.com>
2025-04-22 21:36:04 -0300

    Add files via upload

---
+++ back.js
@@ -0,0 +1,6 @@
+let nome = 'Thiago';
+console.log(nome);
+console.log('resultado 1',(7 != 7) && (2 > 3));
+console.log('resultado 2',(2 == 5) && (7 < 5));
+console.log('resultado 3',(3 >= 1) && (10 < 2));
+console.log('resultado 4',(5 == 5) && (2 == 2));
